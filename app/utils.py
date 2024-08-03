52842204815e5a97a575ad9e6596dfb256d9afde
2W <hsp003636@gmail.com>
2024-08-03 11:53:29 +0900

    KAN-288 <fix : file download method modified to stream

--- app/utils.py
+++ app/utils.py
@@ -17,45 +17,6 @@ from app import (
 )
 from app.models import FileScanRequest
 
-s3 = boto3.client("s3")
-
-
-def compile_yara_rules(directories):
-    rule_sources = {}
-
-    # 리스트 형태로 제공된 여러 디렉토리를 순회합니다.
-    for directory in directories:
-        # 디렉토리 내의 모든 .yar 파일을 찾습니다.
-        yara_files = [
-            os.path.join(directory, f)
-            for f in os.listdir(directory)
-            if f.endswith(".yar")
-        ]
-        for yara_file in yara_files:
-            with open(yara_file, "r") as file:
-                # 중복된 파일 이름이 있는지 체크하고 파일 내용을 읽습니다.
-                if yara_file in rule_sources:
-                    continue  # 이미 같은 이름의 파일이 추가된 경우 덮어쓰기를 피합니다.
-                rule_sources[yara_file] = file.read()
-
-    # 모든 규칙을 컴파일합니다.
-    compiled_rules = yara.compile(sources=rule_sources)
-    return compiled_rules
-
-
-def ensure_utf8_encoding(file_path):
-    try:
-        with open(file_path, "rb") as f:
-            raw_data = f.read()
-        raw_data.decode("utf-8")
-        logging.info(f"{file_path} is already UTF-8 encoded.")
-    except UnicodeDecodeError:
-        logging.info(f"{file_path} is not UTF-8 encoded. Converting to UTF-8.")
-        with open(file_path, "r", encoding="latin1") as f:
-            content = f.read()
-        with open(file_path, "w", encoding="utf-8") as f:
-            f.write(content)
-
 
 def load_yara_rules(rules_dir):
     filepaths = {}
@@ -63,7 +24,6 @@ def load_yara_rules(rules_dir):
         for file in files:
             if file.endswith(".yar"):
                 file_path = os.path.join(root, file)
-                ensure_utf8_encoding(file_path)
                 filepaths[file] = file_path
 
     if not filepaths:
@@ -80,7 +40,7 @@ def load_yara_rules(rules_dir):
         return None
 
 
-def download_file_from_s3(file_path, s3_key):
+def stream_file_from_s3(s3_key):
     s3_client = boto3.client(
         "s3",
         aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -90,13 +50,10 @@ def download_file_from_s3(file_path, s3_key):
     key = "/".join(s3_key.split("/")[1:])
 
     try:
-        # logging.info(
-        #     f"Attempting to download file from S3. Bucket: {bucket_name}, Key: {key}, Local Path: {file_path}"
-        # )
-        s3_client.download_file(bucket_name, key, file_path)
-        logging.info(f"File downloaded successfully from S3: {key}")
+        response = s3_client.get_object(Bucket=bucket_name, Key=key)
+        return response["Body"]
     except Exception as e:
-        logging.error(f"Failed to download file from S3: {e}")
+        logging.error(f"Failed to stream file from S3: {e}")
         raise
 
 
@@ -118,73 +75,41 @@ def save_scan_result(file_id, detect, detail):
     conn.close()
 
 
-def scan_file(file_id: int, yara_rules):
-    logging.info(f"rules : {yara_rules}")
-    file_path = None  # Initialize file_path here for use in finally block
+def scan_file(file_id: int, yara_rules, chunk_size=1024 * 1024):  # 1MB 청크
     try:
-        # MySQL에서 파일 정보를 가져옵니다.
-        logging.info("Connecting to MySQL database...")
         conn = mysql.connector.connect(
-            host=MYSQL_HOST,
-            user=MYSQL_USER,
-            password=MYSQL_PASSWORD,
-            database=MYSQL_DB,
+            host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, database=MYSQL_DB
         )
         cursor = conn.cursor(dictionary=True)
-        logging.info(f"Executing query to retrieve file record for file_id: {file_id}")
         cursor.execute("SELECT * FROM stored_file WHERE id = %s", (file_id,))
         file_record = cursor.fetchone()
         cursor.close()
         conn.close()
-        logging.info(f"file_record: {file_record}")
 
         if not file_record:
-            logging.error("File not found in database.")
             raise HTTPException(status_code=404, detail="File not found")
 
-        # S3에서 파일을 다운로드합니다.
-        download_path = os.path.join(os.path.dirname(__file__), "downloads")
-        if not os.path.exists(download_path):
-            os.makedirs(download_path)
-
-        file_path = os.path.join(
-            download_path, os.path.basename(file_record["save_path"])
-        )
-        print(file_path)
         s3_key = file_record["save_path"]
-        # logging.info(
-        #     f"Downloading file from S3 with key: {s3_key} to path: {file_path}"
-        # )
+        file_stream = stream_file_from_s3(s3_key)
 
-        try:
-            download_file_from_s3(file_path, s3_key)
-        except Exception as e:
-            logging.error(f"Failed to download file from S3: {e}")
-            raise HTTPException(
-                status_code=500, detail="Failed to download file from S3"
-            )
+        matches = []
+        while True:
+            chunk = file_stream.read(chunk_size)
+            if not chunk:
+                break
+            chunk_matches = yara_rules.match(data=chunk)
+            matches.extend(chunk_matches)
 
-        # YARA를 사용하여 파일을 검사합니다.
-        logging.info(f"Scanning file at path: {file_path} with YARA rules.")
-        target_file_path = os.path.join(download_path, s3_key.split("/")[-1])
-        matches = yara_rules.match(target_file_path)
-        logging.info(f"Scan result: {matches}")
         detect = 1 if matches else 0
-        detail = "\n".join([str(match) for match in matches])
-        logging.info(
-            f"Scan result for file {file_id}: detect={detect}, detail={detail}"
+        detail = (
+            "\n".join([str(match) for match in matches]) if matches else "unmatched"
         )
 
-        # 결과를 MySQL에 저장합니다.
+        logging.info(f"result: {matches}")
+        logging.info(f"detect: {detect}")
+        logging.info(f"detail: {detail}")
+
         save_scan_result(file_id, detect, detail)
     except Exception as e:
         logging.error(f"Error scanning file: {e}")
         raise HTTPException(status_code=500, detail="Error scanning file")
-    finally:
-        # 로컬에 다운로드한 파일을 삭제합니다.
-        try:
-            if file_path and os.path.exists(file_path):
-                logging.info(f"Deleting local file at path: {file_path}")
-                os.remove(file_path)
-        except Exception as e:
-            logging.error(f"Failed to delete local file: {e}")
